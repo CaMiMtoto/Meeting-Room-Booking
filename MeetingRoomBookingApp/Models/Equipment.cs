7f283ea74f3c99939e08153206c0f34a7e918000
Jean Paul Byiringiro <jeanpaulcami@live.com>
2022-08-01 12:22:02 +0200

    Initial

--- MeetingRoomBookingApp/Models/Equipment.cs
+++ MeetingRoomBookingApp/Models/Equipment.cs
@@ -3,8 +3,15 @@ namespace MeetingRoomBookingApp.Models
 {
     public class Equipment
     {
-        public Equipment()
-        {
-        }
+       
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool BookMultiUnits { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string BookingOption { get; set; }
     }
 }
